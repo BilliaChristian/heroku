a8c7354f0a22cdc683647e5ab69f532b78a725c3
BilliaChristian <c.billia.9623@vallauri.edu>
2020-05-14 18:12:27 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,6 @@ const ERRORS = require('errors');
 const path = require('path');
 const express = require("express");
 const bodyParser = require('body-parser');
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
 var serveStatic = require('serve-static');
 
