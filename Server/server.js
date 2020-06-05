c0dd40e8038ae71f3ea36a0c0714ee17024dd8d7
BilliaChristian <c.billia.9623@vallauri.edu>
2020-06-05 14:33:16 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -467,8 +467,10 @@ app.post("/api/newLeader",function (req,res) {
                 const DB = client.db('App');
                 let collection = DB.collection('user');
                 collection.updateOne({ "_id.nome": idUser[0], "_id.cognome": idUser[1], "_id.codice": parseInt(idUser[2]) }, { $set: { "team.nome": nomeTeam, "team.idLeade": idLeader, "team.ruolo": "C", "team.stato": "Attivo" ,"tipo":"T"} }, function (err, result) {
-                    if (err)
+                    if (err){
+                        console.log(err);
                         res.send({ "ris": "err" });
+                    }
                     else
                         res.send({ "ris": "ok" });
                 });
